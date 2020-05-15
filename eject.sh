698961843958d4513799a4262cb47da6bd9284b0
pascalbrahma <44594524+pascalbrahma@users.noreply.github.com>
2020-05-15 11:46:00 +0700

    Update eject.sh
    Adding notify-send support, prompt for install worked correctly.

--- eject.sh
+++ eject.sh
@@ -2,6 +2,7 @@
 #set -x
 # Ejecting drive seamlessly
 UDISKS="/usr/bin/udisksctl"
+NOTIFY="/usr/bin/notify-send"
 # Define help function
 function print_help () {
   echo "switch: [-h|-?|--h|--help] [-i|--interactive]"
@@ -11,10 +12,32 @@ function print_help () {
   echo "Toggle --interactive switch to automate task"
 }
 
+function notify_unmount () {
+  if command -v notify-send >/dev/null 2>&1; then
+    DISPLAY=:0 notify-send -i '/usr/share/icons/gnome/32x32/devices/drive-removable-media.png' "Unmounting $1"
+  else
+    echo "Unmounting $1"
+  fi
+}
+
+function notify_eject () {
+  if command -v notify-send >/dev/null 2>&1; then
+    DISPLAY=:0 notify-send -i '/usr/share/icons/gnome/32x32/devices/drive-optical.png' "You can now unplug $1 safely"
+  else
+    echo "Powering off $1"
+  fi
+}
+
 function package_check () {
-  command -v udisksctl >/dev/null 2>&1 || { echo >&2 \
-        "udisks2 package required but it is not installed.  Aborting. \
-        You can install it by typing sudo apt-get install udisks2"; }
+  command -v udisksctl >/dev/null 2>&1 || { read -p >&2 \
+        "udisks2 package required but it is not installed. \
+        Do you want to install it ?" yn; echo
+        case $yn in
+          [Yy]* ) sudo apt-get install udisks2; break ;;
+          [Nn]* ) exit 2;;
+          * ) echo "Please answer Y or N";;
+        esac;
+      }
 }
 
 function interactive () {
@@ -26,7 +49,7 @@ function interactive () {
   # Print all drive excluding the one Ubuntu is installed to, store them on array
   oldIFS=$IFS; IFS=$'\n' drive=( $(mount | grep "/dev/sd*[a-z][0-9]" | grep -v "$current_drive" | sed 's/type.*//') ); IFS=$oldIFS
   if [ ${#drive[@]} -eq 0 ]; then
-    echo "External drive not detected !"
+    echo "No external drive parititon found ! If you have one, mount it first"
   else
     echo "============================================="
     echo "Please select your drive name to be ejected"
@@ -38,9 +61,8 @@ function interactive () {
       [[ $drives = Cancel ]] && echo "Bye !" && break
       local DRIVE_PARTITION=$(echo $drives | sed 's/on.*//' | sed 's/ //g')
       local DRIVE_NAME=$(tr -d 0-9 <<<$DRIVE_PARTITION)
-      echo "Ejecting $DRIVE_PARTITION from system"
-      $UDISKS unmount -b "$DRIVE_PARTITION" >/dev/null
-      $UDISKS power-off -b "$DRIVE_NAME"
+      { $UDISKS unmount -b $DRIVE_PARTITION >/dev/null ; } && notify_unmount "$DRIVE_PARTITION"
+      $UDISKS power-off -b "$DRIVE_NAME" && notify_eject "$DRIVE_NAME"
       echo "Done !" && break
     done
   fi
@@ -62,6 +84,7 @@ while :; do
 done
 
 if [ $# -gt 0 ]; then
+  package_check
   DRIVE_NAME="$1"
   current_drive=$(findmnt -n -o SOURCE --target / | tr -d 0-9)
   if [ ! -b "$DRIVE_NAME" ]; then
@@ -78,11 +101,11 @@ if [ $# -gt 0 ]; then
   IFS=${OLDIFS}
 
   for partition in "${drive[@]}"; do
-    { $UDISKS unmount -b $partition >/dev/null ; } && echo "Unmounting $partition"
+    { $UDISKS unmount -b $partition >/dev/null ; } && notify_unmount $partition
   done
-  $UDISKS power-off -b $DRIVE_NAME && echo "Powering off $DRIVE_NAME"
+  $UDISKS power-off -b $DRIVE_NAME && notify_eject $DRIVE_NAME
 else
-  echo "Missing option !!"
+  echo "No option specified !!"
   echo "Execute ejecting --help to display usage"
   exit 1
 fi
